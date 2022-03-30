b43b304016aa505737625b9ed899a311bf14fc9b
Pypochek <gohavelik@gmail.com>
2022-03-30 19:14:20 +0300

    aasd

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const button = document.querySelector(".menu-icon");
 
 button.addEventListener("click", function () {
-  document.querySelector("header").style.backgroundColor = "rgb(255, 244, 221)";
+  document.querySelector("header").style.backgroundColor = "black";
   document.querySelector("header").style.display = "block";
 
   document.querySelector("header").style.paddingBottom = "100%";
 
   document.querySelector(".menu").style.display = "block";
-  document.querySelector(".menu").style.marginTop = "20%";
+  document.querySelector(".menu").style.marginTop = "30%";
 
-  document.querySelector(".hr1").style.marginBottom = "65px";
+  document.querySelector(".hr1").style.marginTop = "50px";
 });
 
 // document.querySelector(".hr1").style.fontSize = "35px";
