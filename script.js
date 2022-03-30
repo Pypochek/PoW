cd4bc290013d9f768fcf31c60cde041af9420196
Pypochek <gohavelik@gmail.com>
2022-03-30 11:52:02 +0300

    sa

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const button = document.querySelector(".menu-icon");
 
 button.addEventListener("click", function () {
-  document.querySelector("header").style.backgroundColor = "black";
+  document.querySelector("header").style.backgroundColor = "rgb(255, 244, 221)";
   document.querySelector("header").style.display = "block";
+  document.querySelector("header").style.marginBottom = "100%";
+  document.querySelector("header").style.paddingBottom = "100%";
+
+  document.querySelector(".menu").style.display = "block";
+  document.querySelector(".hr").style.marginLeft = "60px";
+  document.querySelector(".hr").style.marginTop = "70px";
+  document.getElementsByClassName(".hr").style.fontSize = "35px";
+
+  // Пофиксить применение чтиля только к одному елементу
 });
