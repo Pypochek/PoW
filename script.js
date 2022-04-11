0fd63f5d20b17a17c5059d546a9c9199a4e7c2a7
Pypochek <gohavelik@gmail.com>
2022-04-11 19:58:33 +0300

    fixed menu

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//
+//
+//
+//
+//
+//  Add and remove styles from objects on click
+//  Добавление и удаление стилей у обьектов при нажатии кнопок
+//
 const button = document
   .querySelector(".menu-icon")
   .addEventListener("click", function () {
@@ -38,11 +46,42 @@ const buttonClose = document
     document.querySelector(".players-list").style.display = "";
   });
 
+//
+//
+//
+//
+// Copy text on click at stroke
+// Копирование текста при нажатии на строчку
+
 const copiedText = document.querySelector(".ip");
-// copiedText.addEventListener("click", function () {
-//   window.navigator.clipboard.writeText("discord.gg/xMJQapRBYe");
-// });
 
 copiedText.addEventListener("click", function () {
   window.open("https://discord.gg/xMJQapRBYe");
 });
+
+//
+//
+//
+// Sticky menu
+// Липкое меню
+
+let header = document.getElementById("headPage");
+let sticky = header.offsetTop;
+
+function ScrollFunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky-header");
+    document.querySelector(".name").style.paddingRight = "40px";
+    document.querySelector(".name-bitshort").style.paddingRight = "40px";
+    document.querySelector(".header").style.paddingBottom = "80px";
+  } else {
+    header.classList.remove("sticky-header");
+    document.querySelector(".name").style.paddingRight = "0";
+    document.querySelector(".name-bitshort").style.paddingRight = "0";
+    document.querySelector(".header").style.paddingBottom = "60px";
+  }
+}
+
+window.onscroll = function () {
+  ScrollFunction();
+};
